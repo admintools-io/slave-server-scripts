cc1a62206d411b77f0cbc73fb1598b7a870d6d67
Juan Navas <navascruzf@gmail.com>
2021-01-20 15:45:31 -0400

    update

--- setting-up-docker.sh
+++ setting-up-docker.sh
@@ -3,7 +3,7 @@
 if [ -f /etc/redhat-release ] ; then
 	yum update -y
 elif [ -f /etc/debian_version ] ; then
-bigecho "Upgrading system and setting up docker"
+echo "Upgrading system and setting up docker"
 apt-get update && apt-get -y upgrade -y
 apt-get install -y docker.io git
 #cat > /etc/docker/daemon.json <<EOF
